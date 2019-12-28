d29a8b883049534d460d4a52f98c0f92fdd63ced
spaceranger-lightyear <lightyear.thespaceranger@gmail.com>
2019-12-28 13:01:30 +0900

    SOLVED!

--- Algora/Week7/7-3(7576).java
+++ Algora/Week7/7-3(7576).java
@@ -1,25 +1,62 @@
 import java.util.*;
-import java.io.*;
 
 public class Main {
     private static final Scanner kb = new Scanner(System.in);
-    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static int [][] dir = { {1, 0}, {0, 1}, {-1, 0}, {0, -1} };
     
-    private static int arr[][];
-    public static void main(String args[]) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str1[] = br.readLine().split(" ");
+    public static void main(String args[]) {
+        int W = kb.nextInt(), H = kb.nextInt();
+        int [][] box = new int [H][W];
         
-        int arr[] = new int[3];
+        for( int i = 0; i < H; i++ )
+            for( int j = 0; j < W; j++ )
+                box[i][j] = kb.nextInt();
         
-        for (int i = 0; i < 3; i++) arr[i] = Integer.parseInt(str1[i]);
+        BFS(box, H, W);
+    }
+    
+    public static void BFS(int [][] box, int h, int w) {
+        Queue<Point> q = new LinkedList<Point>();
         
-        Arrays.sort(arr);
-        String str2[] = br.readLine().split("");
+        for( int i = 0; i < h; i++ )
+            for( int j = 0; j < w; j++ )
+                if( box[i][j] == 1 )
+                    q.add(new Point(i, j));
         
-        for (int i = 0; i < 3; i++) bw.write(arr[str2[i].charAt(0) - 'A'] + " ");
+        while( !q.isEmpty() ) {
+            Point point = q.poll();
+            
+            for( int i = 0; i < 4; i++ ) {
+                int nextX = point.x + dir[i][0];
+                int nextY = point.y + dir[i][1];
+                
+                if( nextX < 0 || nextX >= h || nextY < 0 || nextY >= w ) continue;
+                if( box[nextX][nextY] != 0 ) continue;
+                
+                box[nextX][nextY] = box[point.x][point.y] + 1;
+                q.add(new Point(nextX, nextY));
+            }
+        }
         
-        bw.flush();
+        int max = 0;
+        
+        for( int i = 0; i < h; i++ )
+            for( int j = 0; j < w; j++ ) {
+                if( box[i][j] == 0) {
+                    System.out.println(-1);
+                    return;
+                }
+                max = Math.max(max, box[i][j]);
+            }
+        System.out.println(max - 1);
+    }
+}
+
+class Point {
+    int x, y;
+    
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
 }
