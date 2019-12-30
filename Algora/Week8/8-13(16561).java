3d7d39a826ce4f731fe99e27ed2756e3a31066b6
spaceranger-lightyear <lightyear.thespaceranger@gmail.com>
2019-12-30 19:13:57 +0900

    SOLVED!

--- Algora/Week8/8-13(16561).java
+++ Algora/Week8/8-13(16561).java
@@ -6,14 +6,24 @@ public class Main {
     private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     
     public static void main(String args[]) throws IOException {
-        int N = kb.nextInt();
+        int ret, n = kb.nextInt();
         
-        for( int i = 1; i <= N; i++ ) {
-            for( int j = i; j < N; j++ )
-                System.out.print(" ");
-            for( int k = 1; k <= 2*i - 1; k++ )
-                System.out.print("*");
-            System.out.println();
+        if( n < 9 ) ret = 0;
+        else if( n == 9 ) ret = 1;
+        else {
+            n -= 9;
+            n /= 3;
+            ret = dfs(n, 2);
         }
+        System.out.println(ret);
+    }
+    
+    public static int dfs(int num, int cnt) {
+        if( cnt == 0 ) return 1;
+        int ret = 0;
+        for( int i = num; i >= 0; i-- ){
+            ret += dfs(i, cnt - 1);
+        }
+        return ret;
     }
 }
