7d68976a42a93a850dc0e1543bf4793a5a37aecd
Jacob An <lightyear.thespaceranger@gmail.com>
2020-01-15 19:18:40 +0900

    SOLVED!

--- Algora/Week10/10-6(9095).java
+++ Algora/Week10/10-6(9095).java
@@ -6,16 +6,24 @@ public class Main {
 	private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 	public static void main(String args[]) throws IOException{
-		int N = Integer.parseInt(br.readLine());
-		long [] DP = new long [N+1];
+		int T = Integer.parseInt(br.readLine());
 
-		DP[0] = 0; DP[1] = 1; DP[2] = 2;
+		for( int i = 0; i < T; i++ ) {
+			int n = Integer.parseInt(br.readLine());
+			long [] DP = new long [n+1];
 
-		for( int i = 3; i < N+1; i++ ) {
-			DP[i] = DP[i-1] + DP[i-2];
-			DP[i] %= 15746;
+			for( int j = 0; j <= n; j++ )
+				DP[j] = solve(j);
+
+			bw.write(Long.toString(DP[n]) + "\n");
+			bw.flush();
 		}
-		bw.write(Long.toString(DP[N]));
-		bw.flush();
+	}
+	
+	public static int solve(int n) {
+		if( n < 0 ) return 0;
+		if( n == 0 ) return 1;
+		
+		return solve(n-1) + solve(n-2) + solve(n-3);
 	}
 }
