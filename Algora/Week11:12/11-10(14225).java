f73c9f9371b9b0069c294d806c367dc080148c8c
Jacob An <lightyear.thespaceranger@gmail.com>
2020-01-26 16:52:34 +0900

    unSOLVED

--- Algora/Week11:12/11-10(14225).java
+++ Algora/Week11:12/11-10(14225).java
@@ -6,23 +6,31 @@ public class Main {
 	private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 	public static int N, answer;
-	public static ArrayList<Integer> permutation;
+	public static ArrayList<Integer> set;
 	public static void main(String args[]) throws IOException {
 		N = Integer.parseInt(br.readLine());
 
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		permutation = new ArrayList<>();
+		set = new ArrayList<>();
 
 		for( int i = 0; i < N; i++ )
-			permutation.add(Integer.parseInt(st.nextToken());
+			set.add(Integer.parseInt(st.nextToken()));
 
-		backtrack(0);
+		set.sort(new pComparator());
 
-		bw.write(Integer.toString(answer));
+		bw.write("");
 		bw.flush();
 	}
 
 	public static void backtrack(int index) {
 	}
+}
 
+class pComparator implements Comparator<Integer> {
+	@Override
+	public int compare(Integer o1, Integer o2) {
+		// TODO Auto-generated method stub
+		if( o1 > o2 ) return 1;
+		return -1;
+	}
 }
