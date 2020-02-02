6376535851a74b2c01c47ea8f16ef4dae45a93ba
Jacob An <lightyear.thespaceranger@gmail.com>
2020-02-02 19:27:11 +0900

    unSOLVED

--- Algora/Week11:12/11-11(16198).java
+++ Algora/Week11:12/11-11(16198).java
@@ -5,14 +5,24 @@ public class Main {
 	private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+	public static ArrayList<Integer> energy;
 	public static void main(String args[]) throws IOException {
 	int N = Integer.parseInt(br.readLine());
-	int [] energy = new int [N];
+	energy = new ArrayList<Integer>();
 
 	StringTokenizer st = new StringTokenizer(br.readLine());
-	for( int i = 0; i < N; i ++ )
-		energy[i] = Integer.parseInt(st.nextToken());
+	for( int i = 0; i < N; i++ )
+		energy.add(Integer.parseInt(st.nextToken()));
 	
+	System.out.println(getEnergy(0,0));
+	}
 	
+	public static int getEnergy(int index, int sum) {
+		if( index == 0 || index == energy.size() ) return 0;
+		sum += energy.get(index-1) * energy.get(index+1);
+		energy.remove(index-1);
+		energy.remove(index+1);
+		
+		return sum + getEnergy(index+1, sum);
 	}
 }
