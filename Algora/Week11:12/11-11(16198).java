e9dc06d534490500a8908cc26cd96a845163a9b9
Jacob An <lightyear.thespaceranger@gmail.com>
2020-02-02 19:20:01 +0900

    unSOLVED

--- Algora/Week11:12/11-11(16198).java
+++ Algora/Week11:12/11-11(16198).java
@@ -5,37 +5,14 @@ public class Main {
 	private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-	public static int N, answer;
-	public static boolean [] visited;
-	public static int [] set;
 	public static void main(String args[]) throws IOException {
-		N = Integer.parseInt(br.readLine());
+	int N = Integer.parseInt(br.readLine());
+	int [] energy = new int [N];
 
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		set = new int [20+1];
-
-		for( int i = 0; i < N; i++ )
-			set[i] = Integer.parseInt(st.nextToken());
-
-		visited = new boolean [2000000+1];
-
-		backtrack(0, 0);
-		
-		for( int i = 0; i < 2000000+1; i++ )
-			if( !visited[i] ) {
-				bw.write(Integer.toString(i));
-				bw.flush();
-				break;
-			}
-	}
-
-	public static void backtrack(int n, int sum) throws IOException {
-		if( n == N ) {
-			visited[sum] = true;
-			return;
-		}	
-		
-		backtrack(n+1, sum+set[n]);
-		backtrack(n+1, sum);
+	StringTokenizer st = new StringTokenizer(br.readLine());
+	for( int i = 0; i < N; i ++ )
+		energy[i] = Integer.parseInt(st.nextToken());
+	
+	
 	}
 }
