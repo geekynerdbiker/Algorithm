2d67da94ec1fc63a6a0d85f187ff64096873f946
Jacob An <lightyear.thespaceranger@gmail.com>
2020-02-07 23:16:44 +0900

    unSOLVED

--- Algora/Week13/13-2(10703).java
+++ Algora/Week13/13-2(10703).java
@@ -6,47 +6,26 @@ public class Main {
 	private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 	public static void main(String args[]) throws IOException{
-		int T = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int R = Integer.parseInt(st.nextToken()), S = Integer.parseInt(st.nextToken());
+		char [][] map = new char [R][S];
+		for( int i = 0; i < R; i++ )
+			map[i] = br.readLine().toCharArray();
 
-		for( int i = 0; i < T; i++ ) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int m = Integer.parseInt(st.nextToken());
-			int n = Integer.parseInt(st.nextToken());
-			int x = Integer.parseInt(st.nextToken());
-			int y = Integer.parseInt(st.nextToken());
+		int depth = 0;
+		for( int i = 0; i < S; i++ )
+			depth = Math.max(depth, findDepth(map, 0, i, 0, R));
 
-			int gcb = GCB(m, n);
-			int lcm = m * n / gcb;
-			int year = -1;
-
-			int lDiff = x - 1;
-			int rDiff = y - 1;
-
-			for (int j = 0; (m * j) < lcm; j++) { 
-				int mulX = m * j;
-				int cur = mulX + lDiff + 1;
-				int right = (cur - 1) % n + 1;
-				
-				if (right == y)
-					year = cur; 
-			}
-			bw.write(Integer.toString(year) + "\n");
-			bw.flush();
-		}
+		System.out.println(depth);
+		for( int i = 0; i < R; i++ )
+			System.out.println(map[i]);
 	}
-	
-	public static int GCB(int a, int b) {
-		if( a > b ) {
-			if( a % b != 0 )
-				return GCB(b, a % b);
-			else
-				return b;
-		}
-		else {
-			if( b % a != 0 )
-				return GCB(a, b % a);
-			else
-				return a;
-		}
+
+	public static int findDepth(char [][] map, int depth, int indexX, int indexY, int R) {
+
+		if( map[indexY][indexX] == 'X' )
+			if( map[indexY+1][indexX] == 'X' )
+				findDepth(map, depth+1, indexX, indexY+1, R);
+		return depth;
 	}
 }
