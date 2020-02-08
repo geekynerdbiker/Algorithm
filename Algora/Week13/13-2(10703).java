371cfd9143a18de59e0cb2dc547302b17905325c
Jacob An <lightyear.thespaceranger@gmail.com>
2020-02-08 22:23:35 +0900

    unSOLVED

--- Algora/Week13/13-2(10703).java
+++ Algora/Week13/13-2(10703).java
@@ -8,24 +8,32 @@ public class Main {
 	public static void main(String args[]) throws IOException{
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int R = Integer.parseInt(st.nextToken()), S = Integer.parseInt(st.nextToken());
-		char [][] map = new char [R][S];
-		for( int i = 0; i < R; i++ )
-			map[i] = br.readLine().toCharArray();
+		//char [][] map = new char [R][S];
+		ArrayList<char []> map = new ArrayList<char []>();
 
-		int depth = 0;
-		for( int i = 0; i < S; i++ )
-			depth = Math.max(depth, findDepth(map, 0, i, 0, R));
+		for( int i = 0; i < R; i++ ) {
+			char [] line = br.readLine().toCharArray();
+			map.add(line);
+		}
 
-		System.out.println(depth);
+		while( findEmptySky(map, R, S) > -1 ) {
+			
+		}
+
 		for( int i = 0; i < R; i++ )
-			System.out.println(map[i]);
+			System.out.println(map.get(i));
 	}
 
-	public static int findDepth(char [][] map, int depth, int indexX, int indexY, int R) {
+	public static int findEmptySky(ArrayList<char []> arr, int R, int S) {
+		for( int i = 0; i < R; i++ )
+			for( int j = 0; j < S; j++ ) {
+				if( arr.get(i)[j] != '.') break;
+				if( j == S - 1 && arr.get(i)[j] == '.' ) return i;
+			}
+		return -1;
+	}
 
-		if( map[indexY][indexX] == 'X' )
-			if( map[indexY+1][indexX] == 'X' )
-				findDepth(map, depth+1, indexX, indexY+1, R);
-		return depth;
+	public static void dropComet(ArrayList<char []> arr, int R, int S, int index) {
+
 	}
 }
