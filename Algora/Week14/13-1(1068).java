5299b0d9e90524e0a8233f2d5b9fc537ba3807b1
Jacob An <lightyear.thespaceranger@gmail.com>
2020-02-11 17:29:08 +0900

    unSOLVED

--- Algora/Week14/13-1(1068).java
+++ Algora/Week14/13-1(1068).java
@@ -1,29 +1,19 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
 	private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-	public static void main(String args[]) throws IOException {
+	public static void main(String args[]) throws IOException{
 		int N = Integer.parseInt(br.readLine());
+		int [] tree = new int [N];
 		
-		for( int i = 2*N-1; i > 0; i -= 2 ) {
-			for( int j = 0; j < 2*N-1-i; j += 2 )
-				bw.write(" ");
-			for( int k = 0; k < i; k++ )
-					bw.write("*");
-				bw.write("\n");
-			}
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for( int i = 0; i < N; i++ )
+			tree[i] = Integer.parseInt(st.nextToken());
 		
-		for( int i = 3; i <= 2*N-1; i += 2 ) {
-			for( int j = 0; j < 2*N-1-i; j += 2 )
-				bw.write(" ");
-			for( int k = 0; k < i; k++ )
-				bw.write("*");
-			bw.write("\n");
-		}
+		int remover = Integer.parseInt(br.readLine());
 		
-		bw.flush();
 	}
 }
