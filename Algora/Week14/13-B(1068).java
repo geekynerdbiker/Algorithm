df24f55e1d60839ff3e2218710937f2416935963
Jacob An <lightyear.thespaceranger@gmail.com>
2020-02-12 20:28:54 +0900

    unSOLVED

--- Algora/Week14/13-B(1068).java
+++ Algora/Week14/13-B(1068).java
@@ -8,12 +8,30 @@ public class Main {
 	public static void main(String args[]) throws IOException{
 		int N = Integer.parseInt(br.readLine());
 		int [] tree = new int [N];
-		
+		boolean [] visited = new boolean [N];
+
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		for( int i = 0; i < N; i++ )
 			tree[i] = Integer.parseInt(st.nextToken());
-		
+
 		int remover = Integer.parseInt(br.readLine());
+
+		removeNode(tree, visited, remover);
 		
+		for(int i = 0; i < N; i++) 
+			System.out.print(tree[i] + " ");
+	}
+
+	public static void removeNode(int [] tree, boolean [] visited, int k) {
+		tree[k] = -2;
+		visited[k] = true;
+		for( int i = 0; i < tree.length; i++ )
+			if( !visited[i] ) {
+				if( tree[i] == k ) {
+					tree[i] = -2;
+					visited[i] = true;
+					removeNode(tree, visited, i);
+				}
+			}
 	}
 }
