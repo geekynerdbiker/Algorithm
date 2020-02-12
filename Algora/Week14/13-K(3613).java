58e00ccfad6d5ab16e1d8bf29c2d2d68de37c3aa
Jacob An <lightyear.thespaceranger@gmail.com>
2020-02-12 21:17:07 +0900

    unSOLVED

--- Algora/Week14/13-K(3613).java
+++ Algora/Week14/13-K(3613).java
@@ -13,25 +13,26 @@ public class Main {
 			if( var[i] < 91 && var[i] >= 65 ) javaFlag = true;
 			if( var[i] == 95 ) cppFlag = true;
 		}
-		
-		if( javaFlag && cppFlag ) {
-			bw.write("Error!");
+
+
+		if( !isValid(var, javaFlag, cppFlag) ) {
+			bw.write("Error!\n");
 			bw.flush();
-			return;
+			//return;
 		}
-		
+
 		StringBuilder sb = new StringBuilder();
 		if( javaFlag ) {
 			int startIndex = 0, endIndex = 0;
-			
+
 			for( int i = 0; i < var.length; i++ ) {
 				if( var[i] < 91 && var[i] >= 65 ) {
 					var[i] += 32;
 					endIndex = i-1;
-					
+
 					for( int j = startIndex; j <= endIndex; j++ )
 						sb.append(var[j]);
-					
+
 					if( endIndex != -1 )
 						sb.append('_');
 					startIndex = i;
@@ -40,17 +41,17 @@ public class Main {
 			for( int i = startIndex; i < var.length; i++ )
 				sb.append(var[i]);
 		}
-		
+
 		else if( cppFlag ) {
 			int startIndex = 0, endIndex = 0;
-			
+
 			for( int i = 0; i < var.length; i++ ) {
 				if( var[i] == 95 ) {
 					endIndex = i;
-					
+
 					for( int j = startIndex; j < endIndex; j++ )
 						sb.append(var[j]);
-					
+
 					startIndex = i+1;
 					var[i+1] -= 32;
 				}
@@ -58,12 +59,26 @@ public class Main {
 			for( int i = startIndex; i < var.length; i++ )
 				sb.append(var[i]);
 		}
-		
+
 		else 
 			for( int i = 0; i < var.length; i++ )
 				sb.append(var[i]);
-		
+
 		bw.write(sb.toString());
 		bw.flush();
+	}
+
+	public static boolean isValid(char [] var, boolean flag1, boolean flag2) {
+		for( int i = 0; i < var.length; i++ ) {
+			if( var[i] < 48 ) return false;	
+			else if( var[i] >= 58 && var[i] < 65 ) return false;
+			else if( var[i] >= 91 && var[i] < 95 ) return false;
+			else if( var[i] == 96) return false;
+			else if( var[i] >= 123 ) return false;
+		}
+
+		if( flag1 && flag2 ) return false;
+		return true;
+
 	}
 }
