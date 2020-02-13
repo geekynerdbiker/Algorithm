14b4f90358b72959c55835866fb406fb0cff06a1
Jacob An <lightyear.thespaceranger@gmail.com>
2020-02-13 16:41:04 +0900

    SOLVED!

--- Algora/Week14/13-J(11727).java
+++ Algora/Week14/13-J(11727).java
@@ -7,81 +7,24 @@ public class Main {
 	private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 	public static void main(String args[]) throws IOException {
-		String s = br.readLine();
-		char [] var = s.toCharArray();
-		boolean javaFlag = false, cppFlag = false;
-
-		for( int i = 0; i < var.length; i++ ) {
-			if( var[i] < 91 && var[i] >= 65 ) javaFlag = true;
-			if( var[i] == 95 ) cppFlag = true;
-		}
-
-
-		if( !isValid(s, javaFlag, cppFlag) ) {
-			bw.write("Error!\n");
-			bw.flush();
-			return;
-		}
-
-		StringBuilder sb = new StringBuilder();
-		if( javaFlag ) {
-			int startIndex = 0, endIndex = 0;
-
-			for( int i = 0; i < var.length; i++ ) {
-				if( var[i] < 91 && var[i] >= 65 ) {
-					var[i] += 32;
-					endIndex = i-1;
-
-					for( int j = startIndex; j <= endIndex; j++ )
-						sb.append(var[j]);
-
-					if( endIndex != -1 )
-						sb.append('_');
-					startIndex = i;
-				}
-			}
-			for( int i = startIndex; i < var.length; i++ )
-				sb.append(var[i]);
-		}
-
-		else if( cppFlag ) {
-			int startIndex = 0, endIndex = 0;
-
-			for( int i = 0; i < var.length; i++ ) {
-				if( var[i] == 95 ) {
-					endIndex = i;
-
-					for( int j = startIndex; j < endIndex; j++ )
-						sb.append(var[j]);
-
-					startIndex = i+1;
-					var[i+1] -= 32;
-				}
-			}
-			for( int i = startIndex; i < var.length; i++ )
-				sb.append(var[i]);
-		}
-
-		else 
-			for( int i = 0; i < var.length; i++ )
-				sb.append(var[i]);
-
-		bw.write(sb.toString());
+		int N = Integer.parseInt(br.readLine());
+		int [] DP = new int [N+1];
+		
+		bw.write(Integer.toString(bottomUp(DP, N)));
 		bw.flush();
 	}
-
-	public static boolean isValid(String s, boolean flag1, boolean flag2) {
-		String p = "^[0-9a-zA-Z_]*$";
-		char [] c = s.toCharArray();
+	
+	public static int bottomUp(int [] arr, int k) {
+		if( k == 0 ) return 0;
 		
-		if( !Pattern.matches(p, s) ) return false;
-		if( c[0] == '_' || c[c.length-1] == '_' ) return false;
-		for( int i = 0; i < c.length-1; i++ )
-			if( c[i] == '_' && c[i+1] == '_') return false;
+		arr[0] = 1;
+		arr[1] = 3;
 		
-		if( c[0] >= 65 && c[0] < 91 ) return false;
-		if( flag1 && flag2 ) return false;
-		return true;
-
-	}
+		for( int i = 2; i < k; i++ ) {
+			arr[i] = arr[i-1] + arr[i-2] * 2;
+			arr[i] %= 10007;
+		}
+		
+		return arr[k-1];
+		}
 }
