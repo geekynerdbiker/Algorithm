9f2152d9ece8214068ec72c98f304e720ac1d589
Jacob An <lightyear.thespaceranger@gmail.com>
2020-02-13 16:54:39 +0900

    unSOLVED

---
+++ Algora/Week14/13-H(2780).java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+import java.util.regex.Pattern;
+
+public class Main {
+	private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+	public static void main(String args[]) throws IOException {
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int KJM = Integer.parseInt(st.nextToken()), LHS = Integer.parseInt(st.nextToken());
+		int round = 0;
+		
+		while ( KJM != LHS ) {
+			KJM = nextNum(KJM);
+			LHS = nextNum(LHS);
+			round++;
+		}
+		
+		bw.write(Integer.toString(round));
+		bw.flush();
+	}
+	
+	public static int nextNum(int k) {
+		if( k % 2 == 0 ) return k / 2;
+		else return k / 2 + 1;
+	}
+}
