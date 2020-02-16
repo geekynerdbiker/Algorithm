4ccda5f3f67e1eb812ea17259a6d86c45754264c
Jacob An <lightyear.thespaceranger@gmail.com>
2020-02-16 21:05:08 +0900

    unSOLVED

--- Level-13 (BackTracking)/assing07(14888).java
+++ Level-13 (BackTracking)/assing07(14888).java
@@ -1,38 +1,21 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
 	private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	
-	public static int N, result = 0;
-	public static int set [] = new int[15];
-	public static boolean visited [] = new boolean [15];
-	
-	public static void main(String args[]) throws IOException {
-		N = Integer.parseInt(br.readLine());
 
-		for( int i = 0; i < N; i++ ) {
-			set[0] = i;
-			backtrack(i, 0);
-		}
-			
-		bw.write(Integer.toString(result));
-		bw.flush();
-	}
-	
-	public static void backtrack(int x, int y) {
-		for( int i = 0; i < y; i++ )
-			if( set[i] == x || Math.abs(x - set[i]) == y - i ) return;
-			
-		if( y == N - 1 ) {
-			result++;
-			return;
-		}
+	public static void main(String args[]) throws IOException {
+		int N = Integer.parseInt(br.readLine());
+		int [] numbers = new int [N];
+		int [] cal = new int [4];
 		
-		for( int i = 0; i < N; i++ ) {
-			set[y+1] = i;
-			backtrack(i, y+1);
-		}
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for( int i = 0; i < N; i++ )
+			numbers[i] = Integer.parseInt(st.nextToken());
+		
+		st = new StringTokenizer(br.readLine());
+		for( int i = 0; i < 4; i++ )
+			cal[i] = Integer.parseInt(st.nextToken());
 	}
 }
