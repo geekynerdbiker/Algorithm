b73b0472c8d52a262993c60e2e9ae7c5068f2a4b
Jacob An <lightyear.thespaceranger@gmail.com>
2020-02-21 16:27:26 +0900

    SOLVED!

--- Algora/Week15/15-8(1302).java
+++ Algora/Week15/15-8(1302).java
@@ -7,19 +7,44 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		int N = Integer.parseInt(br.readLine());
 		
-		Book [] books = new Book[N];
-		
+		boolean registered;
+		ArrayList<Book> books = new ArrayList<Book>();
+
 		for( int i = 0; i < N; i++ ) {
 			String book = br.readLine();
+			registered = false;
+
+			for( int j = 0; j < books.size(); j++ )
+				if( books.get(j).name.equals(book) ) {
+					books.get(j).count++;
+					registered = true;
+					break;
+				}
+			if( !registered ) books.add(new Book(book));
 		}
+
+		books.sort( new ASC() );
+		bw.write(books.get(0).name);
+		bw.flush();
 	}
 }
 
 class Book {
 	int count = 0;
 	String name;
-	
+
 	public Book(String name) {
 		this.name = name;
+	}
+}
+
+class ASC implements Comparator<Book> {
+	@Override
+	public int compare(Book o1, Book o2) {
+		if( o1.count < o2.count ) return 1;
+		if( o1.count == o2.count )
+			if( o1.name.compareTo(o2.name) > 0 ) return 1;
+			else return -1;
+		else return -1;
 	}
 }
