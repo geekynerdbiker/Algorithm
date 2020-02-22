cde578b0dafb360343bd3a322e79e4e1e21328db
Jacob An <lightyear.thespaceranger@gmail.com>
2020-02-22 14:27:48 +0900

    SOLVED!

--- Algora/Week15/15-10(1926).java
+++ Algora/Week15/15-10(1926).java
@@ -4,53 +4,49 @@ import java.util.*;
 public class Main {
 	private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	public static void main(String[] args) throws IOException{
-		int N = Integer.parseInt(br.readLine());
-		ArrayList<Student> students = new ArrayList<Student>();
 
-		for( int i = 0; i < N; i++ ) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			String name = st.nextToken();
-			int day = Integer.parseInt(st.nextToken());
-			int month = Integer.parseInt(st.nextToken());
-			int year = Integer.parseInt(st.nextToken());
+	public static int size;
+	public static void main(String[] args) throws IOException {
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int R = Integer.parseInt(st.nextToken()), C = Integer.parseInt(st.nextToken());
 
-			students.add(new Student(name, day, month, year));
+		int [][] picture = new int [R][C];
+		boolean [][] visited = new boolean [R][C];
+
+		for( int i = 0; i < R; i++ ) {
+			st = new StringTokenizer(br.readLine());
+			for( int j = 0; j < C; j++ ) 
+				picture[i][j] = Integer.parseInt(st.nextToken());
 		}
-		
-		students.sort(new birthComparator());
 
-		bw.write(students.get(0).name + "\n" + students.get(students.size()-1).name);
+		int count = 0, result = 0;
+		for( int i = 0; i < R; i++ )
+			for( int j = 0; j < C; j++ )
+				if( !visited[i][j] && picture[i][j] != 0 ) {
+					size = 0;
+					countMine(picture, visited, i, j);
+					result = Math.max(result, size);
+					count++;
+				}
+        
+		bw.write(Integer.toString(count) + "\n");
+		bw.write(Integer.toString(result));
 		bw.flush();
 	}
-}
 
-class Student {
-	String name;
-	int day;
-	int month;
-	int year;
+	public static void countMine(int [][] arr, boolean [][] visited, int x, int y) {
+		int [][] dir = { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } };
 
-	public Student(String name, int day, int month, int year) {
-		this.name = name;
-		this.day = day;
-		this.month = month;
-		this.year = year;
-	}
-}
+		size++;
+		visited[x][y] = true;
 
-class birthComparator implements Comparator<Student> {
-	@Override
-	public int compare(Student s1, Student s2) {
-		if( s1.year < s2.year ) return 1;
-		else if( s1.year == s2.year ) {
-			if( s1.month < s2.month ) return 1;
-			else if( s1.month == s2.month ) {
-				if( s1.day < s2.day ) return 1;
-				else return -1;
-			}
-			else return -1;
+		for( int i = 0; i < 4; i++ ) {
+			int nextX = x + dir[i][0];
+			int nextY = y + dir[i][1];
+
+			if( nextX >= 0 && nextX < arr.length && nextY >= 0 && nextY < arr[0].length )
+				if( !visited[nextX][nextY] && arr[nextX][nextY] != 0 )
+					countMine(arr, visited, nextX, nextY);	
 		}
-		else return -1;
 	}
 }
