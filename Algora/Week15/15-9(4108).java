a7e85a0bb9189c26af1d070c47f2a02dbf828778
Jacob An <lightyear.thespaceranger@gmail.com>
2020-02-22 11:58:56 +0900

    unSOLVED

--- Algora/Week15/15-9(4108).java
+++ Algora/Week15/15-9(4108).java
@@ -5,46 +5,44 @@ public class Main {
 	private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 	public static void main(String[] args) throws IOException {
-		int N = Integer.parseInt(br.readLine());
-		
-		boolean registered;
-		ArrayList<Book> books = new ArrayList<Book>();
+		while( true ) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int R = Integer.parseInt(st.nextToken()), C = Integer.parseInt(st.nextToken());
 
-		for( int i = 0; i < N; i++ ) {
-			String book = br.readLine();
-			registered = false;
+			if( R == 0 && C == 0 ) break;
+			char [][] map = new char [R][C];
 
-			for( int j = 0; j < books.size(); j++ )
-				if( books.get(j).name.equals(book) ) {
-					books.get(j).count++;
-					registered = true;
-					break;
-				}
-			if( !registered ) books.add(new Book(book));
+			for( int i = 0; i < R; i++ ) {
+				map[i] = br.readLine().toCharArray();
+				for( int j = 0; j < C; j++ ) 
+					if( map[i][j] == '.' ) map[i][j] = '0';
+			}
+
+			for( int i = 0; i < R; i++ )
+				for( int j = 0; j < C; j++ )
+					countMine(map, i, j);
+
+			for( int i = 0; i < R; i++ ) {
+				for( int j = 0; j < C; j++ )
+					bw.write(Character.toString(map[i][j]));
+				bw.write("\n");
+				bw.flush();
+			}
 		}
+	}
 
-		books.sort( new ASC() );
-		bw.write(books.get(0).name);
-		bw.flush();
+	public static void countMine(char [][] map, int x, int y) {
+		int [][] dir = { { 0, 1 }, { 1, 1 }, { 1, 0 }, { 1, -1 }, { 0, -1 }, { -1, -1 }, { -1, 0 }, { -1 , 1 } };
+
+		if( map[x][y] != '*' ) {
+			for( int i = 0; i < 8; i++ ) {
+				int nextX = x + dir[i][0];
+				int nextY = y + dir[i][1];
+
+				if( !(nextX >= 0 && nextX < map.length && nextY >= 0 && nextY < map[0].length) ) continue;
+				if( map[nextX][nextY] == '*' ) map[x][y]++;
+			}
+		}
 	}
 }
 
-class Book {
-	int count = 0;
-	String name;
-
-	public Book(String name) {
-		this.name = name;
-	}
-}
-
-class ASC implements Comparator<Book> {
-	@Override
-	public int compare(Book o1, Book o2) {
-		if( o1.count < o2.count ) return 1;
-		if( o1.count == o2.count )
-			if( o1.name.compareTo(o2.name) > 0 ) return 1;
-			else return -1;
-		else return -1;
-	}
-}
