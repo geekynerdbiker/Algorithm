af3a319c929a76bcc827c9da9b95e70ae1bebd8f
Jacob An <lightyear.thespaceranger@gmail.com>
2020-02-22 12:46:15 +0900

    unSOLVED

--- Algora/Week15/15-9(4108).java
+++ Algora/Week15/15-9(4108).java
@@ -4,44 +4,51 @@ import java.util.*;
 public class Main {
 	private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+	public static int size;
 	public static void main(String[] args) throws IOException {
-		while( true ) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int R = Integer.parseInt(st.nextToken()), C = Integer.parseInt(st.nextToken());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int R = Integer.parseInt(st.nextToken()), C = Integer.parseInt(st.nextToken());
 
-			if( R == 0 && C == 0 ) break;
-			char [][] map = new char [R][C];
+		int [][] picture = new int [R][C];
+		boolean [][] visited = new boolean [R][C];
 
-			for( int i = 0; i < R; i++ ) {
-				map[i] = br.readLine().toCharArray();
-				for( int j = 0; j < C; j++ ) 
-					if( map[i][j] == '.' ) map[i][j] = '0';
-			}
-
-			for( int i = 0; i < R; i++ )
-				for( int j = 0; j < C; j++ )
-					countMine(map, i, j);
-
-			for( int i = 0; i < R; i++ ) {
-				for( int j = 0; j < C; j++ )
-					bw.write(Character.toString(map[i][j]));
-				bw.write("\n");
-				bw.flush();
-			}
+		for( int i = 0; i < R; i++ ) {
+			st = new StringTokenizer(br.readLine());
+			for( int j = 0; j < C; j++ ) 
+				picture[i][j] = Integer.parseInt(st.nextToken());
 		}
+
+		int count = 0;
+		for( int i = 0; i < R; i++ )
+			for( int j = 0; j < C; j++ )
+				if( !visited[i][j] && picture[i][j] != 0 ) {
+					size = 0;
+					countMine(picture, visited, i, j);
+					count++;
+				}
+
+		bw.write(Integer.toString(count) + "\n");
+		bw.write(Integer.toString(size));
+		bw.flush();
 	}
 
-	public static void countMine(char [][] map, int x, int y) {
-		int [][] dir = { { 0, 1 }, { 1, 1 }, { 1, 0 }, { 1, -1 }, { 0, -1 }, { -1, -1 }, { -1, 0 }, { -1 , 1 } };
+	public static void countMine(int [][] arr, boolean [][] visited, int x, int y) {
+		int [][] dir = { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } };
 
-		if( map[x][y] != '*' ) {
-			for( int i = 0; i < 8; i++ ) {
-				int nextX = x + dir[i][0];
-				int nextY = y + dir[i][1];
+		visited[x][y] = true;
 
-				if( !(nextX >= 0 && nextX < map.length && nextY >= 0 && nextY < map[0].length) ) continue;
-				if( map[nextX][nextY] == '*' ) map[x][y]++;
-			}
+		for( int i = 0; i < 4; i++ ) {
+			int nextX = x + dir[i][0];
+			int nextY = y + dir[i][1];
+
+
+			if( nextX >= 0 && nextX < arr.length && nextY >= 0 && nextY < arr[0].length )
+				if( !visited[nextX][nextY] && arr[nextX][nextY] != 0 ) {
+					size++;
+					countMine(arr, visited, nextX, nextY);
+					visited[x][y] = false;
+				}	
 		}
 	}
 }
