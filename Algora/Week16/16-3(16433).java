5d5509fde22d98edeaebd763fa6ce75bf37748b9
Jacob An <lightyear.thespaceranger@gmail.com>
2020-02-23 16:00:09 +0900

    SOLVED!

--- Algora/Week16/16-3(16433).java
+++ Algora/Week16/16-3(16433).java
@@ -5,15 +5,23 @@ public class Main {
 	private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 	public static void main(String[] args) throws IOException{
-		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int R = Integer.parseInt(st.nextToken()) - 1, C = Integer.parseInt(st.nextToken()) - 1;
+	
+		char [][] farm = new char [N][N];
+		
+	
+			for( int i = 0; i < N; i++ )
+				for( int j = 0; j < N; j++ )
+					if( (R+C) % 2 == (i+j) % 2 ) farm[i][j] = 'v';
+					else farm[i][j] = '.';
+	
 		
 		for( int i = 0; i < N; i++ ) {
-			if( i % 2 == 1 ) bw.write(" ");
-			for( int j = 0; j < N; j++ ) {
-				bw.write("*");
-				if( j != N-1 ) bw.write(" ");
-			}
-			if( i != N-1 ) bw.write("\n");
+			for( int j = 0; j < N; j++ )
+				bw.write(farm[i][j]);
+			bw.write("\n");
 		}
 		bw.flush();
 	}
