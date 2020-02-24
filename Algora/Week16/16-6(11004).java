84100c40bcb013a3c38342be1af50cf147008b22
Jacob An <lightyear.thespaceranger@gmail.com>
2020-02-24 22:36:55 +0900

    SOLVED!

--- Algora/Week16/16-6(11004).java
+++ Algora/Week16/16-6(11004).java
@@ -5,29 +5,17 @@ public class Main {
 	private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 	public static void main(String[] args) throws IOException {
-		int N = Integer.parseInt(br.readLine());
-		boolean [] line = new boolean [10000];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken()), index = Integer.parseInt(st.nextToken()) - 1;
+		List<Integer>  arr = new ArrayList<Integer>();
 		
-		int minStart = 0;
-		int maxEnd = 0;
-		while( N-- > 0 ) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int start = Integer.parseInt(st.nextToken()) - 1;
-			int end = Integer.parseInt(st.nextToken()) - 1;
-			
-			minStart = Math.min(minStart, start);
-			maxEnd = Math.max(maxEnd, end);
-			
-			for( int i = start; i < end; i++ )
-				line[i] = true;
-		}
+		st = new StringTokenizer(br.readLine());
+		for( int i = 0; i < N; i++ )
+			arr.add(Integer.parseInt(st.nextToken()));
 		
-		int length = 0;
-		for( int i = minStart; i <= maxEnd; i++ )
-			if( line[i] ) length++;
+		Collections.sort(arr);
 		
-		
-		bw.write(Integer.toString(length));
+		bw.write(Integer.toString(arr.get(index)));
 		bw.flush();
 	}
 }
