6bc97d46f6c7d6f8662b680cda989fbbb04271d9
Jacob An <lightyear.thespaceranger@gmail.com>
2020-02-25 01:09:07 +0900

    unSOLVED

--- Algora/Week16/16-9(2621).java
+++ Algora/Week16/16-9(2621).java
@@ -5,31 +5,47 @@ public class Main {
 	private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 	public static void main(String[] args) throws IOException {
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		long score = Integer.parseInt(st.nextToken());
-		int P = Integer.parseInt(st.nextToken());
-
-		if( N == 0 ) {
-			bw.write("1");
-			bw.flush();
-			return;
-		}
-		long [] board = new long [N];
-
-		int rank = 1;
-		int p = 1;
+		Card [] hand = new Card [5];
 		
-		st = new StringTokenizer(br.readLine());
-		for( int i = 0; i < N; i++ ) { 
-			board[i] = Long.parseLong(st.nextToken());
-			if( board[i] > score ) rank++;
-			if( board[i] >= score ) p++;
+		for( int i = 0; i < 5; i++ ) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			char [] color = st.nextToken().toCharArray();
+			int number = Integer.parseInt(st.nextToken());
+			hand[i] = new Card(color[0], number);
 		}
 		
-		if( rank > P || p > P ) bw.write("-1");
-		else bw.write(Integer.toString(rank));
+		Arrays.sort(hand, new cardComparator());
 		
-		bw.flush();
+		for( int i = 0; i < 5; i++ )
+			System.out.println(hand[i].color + " " + hand[i].number);
+		int score = checkHand(hand);
+		
+	}
+	
+	public static int checkHand(Card [] hand) {
+		int score = 0;
+		int rules = 0;
+		
+		
+		
+		return score;
+	}
+}
+
+class Card {
+	char color;
+	int number;
+	
+	public Card(char c, int n) {
+		this.color = c;
+		this.number = n;
+	}
+}
+
+class cardComparator implements Comparator<Card> {
+	@Override
+	public int compare(Card c1, Card c2) {
+		if( c1.number > c2.number ) return 1;
+		return -1;
 	}
 }
