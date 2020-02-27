33b8be066f066a784f83840c260d63aeda381b80
Jacob An <lightyear.thespaceranger@gmail.com>
2020-02-27 22:10:57 +0900

    unSOLVED

--- Algora/Week16/16-10(2240).java
+++ Algora/Week16/16-10(2240).java
@@ -8,10 +8,38 @@ public class Main {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int T = Integer.parseInt(st.nextToken());
 		int W = Integer.parseInt(st.nextToken());
-		
+
 		int [] plum = new int [T];
-		
-		for( int i = 0; i < T; i++ )
+		int [] DP = new int [1001];
+		int shift = 0, index = 0, start = 0;
+
+		for( int i = 0; i < T; i++ ) {
 			plum[i] = Integer.parseInt(br.readLine());
+
+			if( i == 0 ) {
+				shift = plum[i];
+				start = plum[i];
+			}
+
+			if( shift == plum[i] ) DP[index]++;
+			else {
+				shift = plum[i];
+				index++;
+				DP[index]++;
+			}
+		}
+
+		if( start == 1 )
+			bw.write(Integer.toString(getDP(DP, 0, W)));
+		else
+			bw.write(Integer.toString(Math.max(getDP(DP, 0, W-1), getDP(DP, 1, W+1))));
+		bw.flush();
+	}
+
+	public static int getDP(int [] DP, int start, int end) {
+		int sum = 0;
+		for( int i = start; i <= end; i++ )
+			sum += DP[i];
+		return sum;
 	}
 }
