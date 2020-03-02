58b099a00914b9706698114b464b78c67bb356bc
Jacob An <lightyear.thespaceranger@gmail.com>
2020-03-02 20:56:05 +0900

    unSOLVED

--- Algora/Week17/17-1(11816).java
+++ Algora/Week17/17-1(11816).java
@@ -5,25 +5,26 @@ public class Main {
 	private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 	public static void main(String[] args) throws IOException {
-		char [] number = br.readLine().toCharArray();
+		char [] input = br.readLine().toCharArray();
+		int number = 0;
 		
-		if( number[0] == 0 ) {
-			if( number[1] != 'x' ) {
+		StringBuilder sb = new StringBuilder();
+		if( input[0] == 0 ) {
+			if( input[1] != 'x' ) {
+				for( int i = 1; i < input.length; i++ )
+					sb.append(input[i]);
+				number = 0 + Integer.parseInt(sb.toString());
 				
 			}
-			
 			else {
-				
+				for( int i = 2; i < input.length; i++ )
+					sb.append(input[i]);
+				number = 0x + Integer.parseInt(sb.toString());
 			}
 		}
+		
+		
 		bw.write("");
 		bw.flush();
-	}
-
-	public static int getSum(int [] DP, int start, int end) {
-		int sum = 0;
-		for( int i = start; i <= end; i++ )
-			sum += DP[i];
-		return sum;
 	}
 }
