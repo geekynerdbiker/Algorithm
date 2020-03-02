58b117f0e180a3800d805a8c2df53e5d36a7a11c
Jacob An <lightyear.thespaceranger@gmail.com>
2020-03-02 20:28:00 +0900

    unSOLVED

--- Algora/Week17/17-1(11816).java
+++ Algora/Week17/17-1(11816).java
@@ -4,19 +4,26 @@ import java.util.*;
 public class Main {
 	private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	public static void main(String[] args) throws IOException{
-		int T = Integer.parseInt(br.readLine());
-
-		while( T-- > 0 ) {
-			int N = Integer.parseInt(br.readLine());
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int sum = 0;
-
-			for( int i = 0; i < N; i++ )
-				sum += Integer.parseInt(st.nextToken());
-
-			bw.write(Integer.toString(sum) + "\n");
-			bw.flush();
+	public static void main(String[] args) throws IOException {
+		char [] number = br.readLine().toCharArray();
+		
+		if( number[0] == 0 ) {
+			if( number[1] != 'x' ) {
+				
+			}
+			
+			else {
+				
+			}
 		}
+		bw.write("");
+		bw.flush();
+	}
+
+	public static int getSum(int [] DP, int start, int end) {
+		int sum = 0;
+		for( int i = start; i <= end; i++ )
+			sum += DP[i];
+		return sum;
 	}
 }
