f2cf7b7c94765d981cd65b96384a740d78cf446c
Jacob An <lightyear.thespaceranger@gmail.com>
2020-11-01 15:59:20 +0900

    sovled

---
+++ 1000 to 1999/1402.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        int T = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < T; i++)
+            System.out.println("yes");
+    }
+}
