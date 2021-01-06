2c3a19f2b5f316b12faa688df5813f58f40bd1a0
Jacob An <lightyear.thespaceranger@gmail.com>
2021-01-06 22:50:14 +0900

    unsolved

--- 6000 to 6999/6068.java
+++ 6000 to 6999/6068.java
@@ -7,7 +7,6 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		int N = Integer.parseInt(br.readLine());
-
 		StringTokenizer st = new StringTokenizer(br.readLine());
 	}
 }
