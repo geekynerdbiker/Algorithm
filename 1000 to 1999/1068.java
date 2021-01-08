429af149602f23f4c65d43d5e03650f9e1791011
Jacob An <lightyear.thespaceranger@gmail.com>
2021-01-08 23:28:44 +0900

    unsolved

--- 1000 to 1999/1068.java
+++ 1000 to 1999/1068.java
@@ -3,6 +3,7 @@ import java.util.*;
 
 public class Main {
 	private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	private static final BufferedWriter bw = new BUfferedWriter(new OutputStreamWriter(System.out));
 	
 	public static void main(String args[]) throws IOException{
 		int N = Integer.parseInt(br.readLine());
