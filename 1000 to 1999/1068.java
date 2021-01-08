b23c576937b31ce0f5171f502761cf60ac2a7b98
Jacob An <lightyear.thespaceranger@gmail.com>
2021-01-08 23:27:52 +0900

    Update 1068.java

--- 1000 to 1999/1068.java
+++ 1000 to 1999/1068.java
@@ -3,8 +3,7 @@ import java.util.*;
 
 public class Main {
 	private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
+	
 	public static void main(String args[]) throws IOException{
 		int N = Integer.parseInt(br.readLine());
 		int [] tree = new int [N];
